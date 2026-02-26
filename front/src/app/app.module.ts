64b8a11beb3e75267bc64737b6969b7227d116ec
Scarounine <paulinelesueur93@gmail.com>
2026-02-26 20:05:38 +0100

    feat: add modules and change style

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,16 +7,23 @@ import { HomeComponent } from './pages/home/home.component';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
     CardModule,
-    InputTextModule
+    MenubarModule,
+    InputTextModule,
+    RippleModule,
+    AvatarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
