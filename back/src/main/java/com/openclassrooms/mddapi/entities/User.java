72b5ebce78d06dd908be32075a9c4f346783851b
Scarounine <paulinelesueur93@gmail.com>
2026-03-04 17:59:27 +0100

    feat: create User entity

---
+++ back/src/main/java/com/openclassrooms/mddapi/entities/User.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.mddapi.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
