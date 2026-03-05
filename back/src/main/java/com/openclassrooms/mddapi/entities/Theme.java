3784f8c1288dacf7d061654223d611a29a78c5fc
Scarounine <paulinelesueur93@gmail.com>
2026-03-05 13:47:57 +0100

    feat: create theme repository

--- back/src/main/java/com/openclassrooms/mddapi/entities/Theme.java
+++ back/src/main/java/com/openclassrooms/mddapi/entities/Theme.java
@@ -1,8 +1,13 @@
 package com.openclassrooms.mddapi.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "themes")
 public class Theme {
