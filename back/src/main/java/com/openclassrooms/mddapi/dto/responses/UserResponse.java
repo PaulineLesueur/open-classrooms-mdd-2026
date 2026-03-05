b86cb36e87149372b9f406538a6c37994f6ca0ca
Scarounine <paulinelesueur93@gmail.com>
2026-03-05 15:23:55 +0100

    feat: create User DTOs

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/responses/UserResponse.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.mddapi.dto.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class UserResponse {
+    private UUID id;
+    private String username;
+    private String email;
+}
