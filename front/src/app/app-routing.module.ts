61837deb19a259b010011dc63ea35f974855703b
Scarounine <paulinelesueur93@gmail.com>
2026-03-13 16:32:23 +0100

    feat: create guard and add it to routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -8,18 +8,17 @@ import { ThemesListComponent } from './features/themes/themes-list/themes-list.c
 import { ArticleDetailComponent } from './features/articles/article-detail/article-detail.component';
 import { CreateFormComponent } from './features/articles/create-form/create-form.component';
 import { ProfileComponent } from './features/profile/profile.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
-// consider a guard combined with canLoad / canActivate route option
-// to manage unauthenticated user to access private routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: 'auth/login', component: LoginComponent },
-  { path: 'feed', component: FeedComponent },
-  { path: 'themes', component: ThemesListComponent },
-  { path: 'article/detail', component: ArticleDetailComponent },
-  { path: 'article/create', component: CreateFormComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'feed', component: FeedComponent, canActivate: [AuthGuard] },
+  { path: 'themes', component: ThemesListComponent, canActivate: [AuthGuard] },
+  { path: 'article/detail', component: ArticleDetailComponent, canActivate: [AuthGuard] },
+  { path: 'article/create', component: CreateFormComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
