e5bb9cd7cf0ab8623b31b121ca939a66bb855c5f
Scarounine <paulinelesueur93@gmail.com>
2026-03-13 15:16:48 +0100

    feat: import HttpClientModule + ReactiveFormsModule to app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,7 +42,9 @@ import { SidebarModule } from 'primeng/sidebar';
     DropdownModule,
     PasswordModule,
     SidebarModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
