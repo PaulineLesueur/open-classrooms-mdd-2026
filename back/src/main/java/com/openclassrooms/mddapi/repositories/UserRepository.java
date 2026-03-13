bbdbc6e17d6dcb6a299f5cec6112b2e410477d54
Scarounine <paulinelesueur93@gmail.com>
2026-03-13 13:21:02 +0100

    feat: add UserDetailsServiceImpl with email or username auth

--- back/src/main/java/com/openclassrooms/mddapi/repositories/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import com.openclassrooms.mddapi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    Optional<User> findByEmailOrUsername(String email, String username);
 }
