c96f19db4106021fd7fcf8d00f6fae29ea876d91
Scarounine <paulinelesueur93@gmail.com>
2026-03-13 15:48:26 +0100

    feat: implement login form with reactive form and auth service

--- front/src/app/features/auth/login/login.component.ts
+++ front/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+ form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      identifier: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSubmit(): void  {
+    if (this.form.invalid)
+      return;
+
+    this.authService.login(this.form.value).subscribe({
+      next: (response) => {
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/feed']);
+      },
+      error: (err) => {
+        console.error('Login failed', err);
+      }
+    })
   }
-
 }
