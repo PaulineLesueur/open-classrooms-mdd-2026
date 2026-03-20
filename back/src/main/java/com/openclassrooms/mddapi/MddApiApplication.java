20e0b0c4d6a75697c01ccde4e696d5f732919c58
Scarounine <paulinelesueur93@gmail.com>
2026-03-20 11:51:01 +0100

    docs: add Javadoc to config and exception handler

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -3,9 +3,17 @@ package com.openclassrooms.mddapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point of the MDD API Spring Boot application.
+ */
 @SpringBootApplication
 public class MddApiApplication {
 
+	/**
+	 * Starts the Spring Boot application.
+	 *
+	 * @param args command-line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MddApiApplication.class, args);
 	}
